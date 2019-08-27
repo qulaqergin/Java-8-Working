3309e9496da384b3934c5f9d228d0db70177c746
Ergin Kulak <ekulak@anadolusigorta.com.tr>
2019-08-27 10:29:56 +0300

    27_08_2019 first commit

---
+++ Java-8-Workings/src/part/first_section/FirstProject.java
@@ -0,0 +1,14 @@
+package part.first_section;
+
+public class FirstProject {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Java_Version");
+
+	}
+
+}
